48aeb523eb52fef08b400af0468cf30a2be82e69
Davide <zago-7@live.it>
2021-06-28 15:33:43 +0200

    testing and ignore

--- blast_score.py
+++ blast_score.py
@@ -1,7 +1,9 @@
 
-#takes the bit score dict and the ground truth dict and computes the score for each couple protein - go term
+# takes the bit score dict and the ground truth dict and computes the score for each couple protein - go term
+# score_dict = {cafa_id: {go_term: score}}
 def blast_score(bit_score_dict, gt_dict, go_term_set):
     score_dict = dict()
+    counter = dict
     for k, v in bit_score_dict.items():
         score_dict.setdefault(k, dict())
         for term in go_term_set:
@@ -12,8 +14,18 @@ def blast_score(bit_score_dict, gt_dict, go_term_set):
                 if term in gt_dict[uniprot_id]:
                     n += bit_score
             score_dict[k][term] = n/d 
-    return score_dict
+        prediction_to_text(score_dict, k, "predictions/Blast")
+        score_dict = dict()
+    #return score_dict
         
+def prediction_to_text(score_dict, prot_id, output_path='', n_go_terms=-1):
+    f = open(output_path +'/'+ prot_id + ".txt", "a")
+    for cafa_id, go_dict in score_dict.items():
+        i = 0
+        for go_id, score in sorted(go_dict.items(), key=lambda x: x[1], reverse=True):
+            f.write(cafa_id + "\t" + go_id + "\t" + str(score) + "\n")
+            if i == n_go_terms:
+                break
+            i += 1
+    f.close()
 
-
-    
--- parsers.py
+++ parsers.py
@@ -46,27 +46,76 @@ def parse_ontology(obo_file_path):
 
     return go_terms, alts
 
+# takes a reference file and splits it in a file for each namespace
+# (the uniport ids are also removed because they aren't useful to the evaluation)
+def parse_reference(reference_file):
+    namespaces = dict()
+    with open(reference_file) as rf:
+        for line in rf:
+            cafa_id, go_id, namespace, _ = line.split()
+            if namespace in namespaces:
+                namespaces[namespace].append([cafa_id, go_id])
+            else:
+                namespaces.setdefault(namespace, [[cafa_id, go_id]])
+    rf.close()
+    for ns, ref_list in namespaces.items():
+        f = open(ns+"_gt.txt", "w")
+        for cafa_id, go_id in ref_list:
+            f.write(cafa_id + "\t" + go_id + "\n")
+        f.close()
+
+
 #takes a blast output and returns the corresponding python dictionary
 def parse_blast(blast_file):
-    data = pd.read_csv(blast_file, sep='\t', names=['cafa_id', 'uniprot_id', 'identity', 'alignment_leght',
+    """data = pd.read_csv(blast_file, sep='\t', names=['cafa_id', 'uniprot_id', 'identity', 'alignment_leght',
      'mismatch', 'gap_opens', 'query_start', 'query_end', 'sub_start', 'sub_end', 'e_value', 'bit_score'])
     
     blast_dict = dict()
 
     for row in data.loc[:, ['cafa_id', 'uniprot_id', 'e_value', 'bit_score']].itertuples():
         blast_dict.setdefault(row[1], [])
-        if row[3] > 0.001:
+        if row[3] < 0.001:
             blast_dict[row[1]].append([row[2], row[3], row[4]])
+    return blast_dict"""
+    blast_dict = dict()
+    with open(blast_file) as f:
+        prev_cafa_id = ''
+        count = 0
+        for line in f:
+            cafa_id, uniprot_id, _, _, _, _, _, _, _, _, e_value, bit_score = line.split()
+            ### When having memory problems uncomment ths code and run only this function (will write the prediction file)
+            if prev_cafa_id == '':
+                prev_cafa_id = cafa_id
+            if prev_cafa_id != cafa_id:
+                count += 1
+                if count > 10000:
+                    rev = reverse_dict(blast_dict)
+                    goa_dict = parse_goa("/home/davide/Documenti/training/goa_db_2018_08_exp.dat", rev, ont_dict)
+                    query_dict, gt_dict, go_term_set = return_to_query(goa_dict)
+                    blast_score(query_dict, gt_dict, go_term_set)
+                    count = 0
+                prev_cafa_id = cafa_id
+            if float(e_value) < 0.001:
+                if cafa_id in blast_dict:
+                    blast_dict[cafa_id].append([uniprot_id, float(e_value), float(bit_score)])
+                else:
+                    blast_dict.setdefault(cafa_id, [[uniprot_id, float(e_value), float(bit_score)]])
+        #This aswell
+        rev = reverse_dict(blast_dict)
+        goa_dict = parse_goa("/home/davide/Documenti/training/goa_db_2018_08_exp.dat", rev, ont_dict)
+        query_dict, gt_dict, go_term_set = return_to_query(goa_dict)
+        blast_score(query_dict, gt_dict, go_term_set)
     return blast_dict
 
 # takes a blast dict b_dict={cafa_id: [uniprot_id, e_value, bit_score]} and returns a dictionary
 # using uniprot_id as key uni_dict = {uniprot_id: (go_terms_set,[cafa_id, e_value, bit_score])}
 def reverse_dict(blast_dict):
     rev_dict = dict()
-    for cafa_id, v in blast_dict.items():
-        for uniprot_id, e_value, bit_score in v:
+    for cafa_id in list(blast_dict):
+        for uniprot_id, e_value, bit_score in blast_dict[cafa_id]:
             rev_dict.setdefault(uniprot_id, [set(),[]])
             rev_dict[uniprot_id][1].append([cafa_id, e_value, bit_score])
+        del blast_dict[cafa_id]
     return rev_dict
 
 #Parses a GOA file and adds to the uniprot_dict(obtained in reverse_dict) a set of associated go-terms
@@ -111,25 +160,40 @@ def propagate_terms(go_term_set, ont):
     queue = list(go_term_set)
     while queue:
         term = queue.pop(0)
-        parents = ont[term]
-        for p in parents:
-            go_term_set.add(p)
-            queue.append(p)
+        if term in ont:
+            parents = ont[term]
+            for p in parents:
+                go_term_set.add(p)
+                queue.append(p)
     return go_term_set
 
 start = time.time()         
 ont_dict, _ = parse_ontology("/home/davide/Documenti/training/gene_ontology_edit.obo.2018-08-01")
 blast_dict = parse_blast("/home/davide/Documenti/training/blast_pred.txt")
+"""print("blast parsed")
 rev = reverse_dict(blast_dict)
-rev_go = parse_goa("/home/davide/Documenti/training/goa_db_2018_08_exp.dat", rev, ont_dict)
-query_dict, gt_dict, go_term_set = return_to_query(rev_go)
+blast_dict.clear()
+print("dictionary reversed")
+goa_dict = parse_goa("/home/davide/Documenti/training/goa_db_2018_08_exp.dat", rev, ont_dict)
+rev.clear()
+print("goa parsed")
+query_dict, gt_dict, go_term_set = return_to_query(goa_dict)
+goa_dict.clear()
+print("back to query")
 score_dict = blast_score(query_dict, gt_dict, go_term_set)
+print("score calculation")
+prediction_to_text(score_dict, 100)"""
+#parse_reference("/home/davide/Documenti/training/reference_new.txt")
 
-i=0
-for k, v in sorted(score_dict['T100900016300'].items(), key=lambda p:p[1], reverse=True):
+
+#print(blast_dict['T100900012018'])
+
+"""i=0
+for k, v in sorted(score_dict['T100900012018'].items(), key=lambda p:p[1], reverse=True):
     print(k, v)
     if i == 20:
         break
-    i += 1
+    i += 1"""
 
 print("time elapsed:",time.time()-start)
+
